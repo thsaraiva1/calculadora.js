e2e9e15cf66ed1a2724f44368bc2cdb91a7e27f7
Thiago Saraiva <thiago.delima.saraiva@gmail.com>
2026-02-11 11:57:22 -0300

    Calculadora_v1

---
+++ script.js
@@ -0,0 +1,80 @@
+const visor = document.getElementById("visor");
+const botoes = document.querySelectorAll("button");
+
+// Estado da Calculadora
+
+let numeroAtual = "";
+let numeroAnterior = "";
+let operadorAtual = null;
+
+botoes.forEach((botao) => {
+  botao.addEventListener("click", () => {
+    if (botao.dataset.numero) {
+      numeroAtual += botao.dataset.numero;
+      visor.textContent = numeroAtual;
+    }
+
+    if (botao.dataset.operador) {
+      operadorAtual = botao.dataset.operador;
+      visor.textContent = botao.dataset.operador;
+      numeroAnterior = numeroAtual;
+      numeroAtual = "";
+    }
+
+    if (botao.dataset.acao === "igual") {
+        if (!numeroAnterior || !numeroAtual || operadorAtual === null){
+            return;
+        }
+        
+        const resultado = calcular();
+        
+        visor.textContent = resultado;
+
+        numeroAtual = resultado.toString();
+        numeroAnterior = "";
+        operadorAtual = null;
+
+    }
+
+     if (botao.dataset.acao === "limpar") {
+        numeroAtual = "";
+        numeroAnterior = "";
+        operadorAtual = null;
+        visor.textContent = "0";
+     }
+
+     if (botao.dataset.acao === "apagar" ) {
+        numeroAtual = numeroAtual.slice(0, -1);
+        visor.textContent = numeroAtual;
+
+        if (numeroAtual === "") {
+            visor.textContent = "0";
+        }
+        
+        if (numeroAtual !== "") {
+            visor.textContent = numeroAtual;
+        }
+     }
+    }
+    
+  )
+
+
+function calcular() {
+  const a = parseFloat(numeroAnterior);
+  const b = parseFloat(numeroAtual);
+
+  switch (operadorAtual) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return a / b;
+    default:
+      return null;
+  }
+}
+});
